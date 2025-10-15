6529e80e42b734a91ddce0e0baa3ff7703b4584b
naufalhaidarazmi <naufalhaidar946@gmail.com>
2025-10-15 12:28:26 +0700

    modfikasi pemnambahan jumlah mahasiswa tidak lulus pada siakadfor

--- jobsheet15oct/siakadfor23.java
+++ jobsheet15oct/siakadfor23.java
@@ -8,6 +8,8 @@ public class siakadfor23 {
 
         double nilaitertinggi = 0;
         double nilaiterendah = 100;
+        int jumlahLulus = 0;
+        int jumlahTidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -20,10 +22,18 @@ public class siakadfor23 {
             if (nilai < nilaiterendah) {
                 nilaiterendah = nilai;
             }
+
+            if (nilai >= 60) {
+                jumlahLulus++;
+            } else {
+                jumlahTidakLulus++;
+            }
         }
 
         System.out.println("Nilai tertinggi adalah: " + nilaitertinggi);
         System.out.println("Nilai terendah adalah: " + nilaiterendah);
+        System.out.println("Jumlah mahasiswa lulus: " + jumlahLulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + jumlahTidakLulus);
 
         sc.close();
     }
